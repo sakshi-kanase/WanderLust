514ca5740916a3f3e86240085e3672a0fb906656
sakshi-kanase <kanasesakshi55@gmail.com>
2026-03-01 16:37:24 +0530

    fixed error

--- app.js
+++ app.js
@@ -114,6 +114,12 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { err });
 });
 
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+// app.listen(8080, () => {
+//     console.log("server is listening to port 8080");
+// });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
